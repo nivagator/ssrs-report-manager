65c768cd5ae49248a3f6111eda514f4800858861
Gavin Greer <gavingreer@mac.com>
2017-12-07 22:52:33 -0600

    create report views. add index and report html files.

--- rptmgr/urls.py
+++ rptmgr/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, report, editreport
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:report_id>/', report, name='report'),
+    path('<int:report_id>/edit', editreport, name='editreport'),
 ]
--- rptmgr/views.py
+++ rptmgr/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Report
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the report manager index.")
+    latest_report_list = Report.objects.order_by('report_name')
+    context = {
+        'latest_report_list': latest_report_list,
+    }
+    return render(request, 'rptmgr/index.html', context)
+
+# CRUD - function and class based views
+# Create/Retrieve/Update/Delete
+def report(request, report_id):
+    report = get_object_or_404(Report, pk=report_id)
+    context = {
+        'report': report
+    }
+    return render(request, 'rptmgr/report.html', context)
+    # return HttpResponse("You're looking at report ID #%s." % report_id)
+
+def editreport(request, report_id):
+    return HttpResponse("You're editing report ID #%s." % report_id)
