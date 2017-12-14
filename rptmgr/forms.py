2a3e0fb39e7fc11df102ca52e97a58fa25b2be08
Gavin Greer <gavingreer@mac.com>
2017-12-13 21:52:48 -0600

    add form labels

--- rptmgr/forms.py
+++ rptmgr/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 from .models import Report
 
 class ReportModelForm(forms.ModelForm):
@@ -14,3 +14,8 @@ class ReportModelForm(forms.ModelForm):
             'base_url',
             'active'
         ]
+        labels ={
+            'report_name': _('Report Name'),
+            'report_file_name': _('Report File Name'),
+            'base_url': _('Base Url'),
+        }
